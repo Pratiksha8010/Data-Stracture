67fd9efd80c41ca658dcc07ff5e7d60b0ba4eb5a
Pratiksha <pratikshakale7776@gmail.com>
2023-10-02 07:25:50 +0530

    done

---
+++ Linked List/Doubly Linked List/Doubly_circular/inser_end.c
@@ -0,0 +1,78 @@
+#include<stdio.h>
+#include<stdlib.h>
+typedef struct node
+{
+int data;
+struct node*next,*prev;
+}NODE;
+
+NODE*create(NODE *list)
+{
+    int i,n;
+    NODE *temp,*newnode;
+    printf("\n enter limit for node....");
+    scanf("%d",&n);
+    for(i=0;i<n;i++){
+    newnode=(NODE*)malloc(sizeof(NODE));
+    printf("enter node data....");
+    scanf("%d",&newnode->data);
+    if(list==NULL){
+        temp=list=newnode;
+        newnode->next=list;
+        list->prev=newnode;
+
+    }
+else{
+    temp->next=newnode;
+    newnode->prev=temp;
+    temp=newnode;
+    temp->next=list;
+    list->prev=temp;
+}
+    }
+   return list;
+}
+
+void *disp(NODE *list)
+{
+NODE *temp=list;
+
+do{
+    printf("%d\t",temp->data);
+temp=temp->next;
+}while(temp!=list);
+
+    
+}
+NODE*insertend(NODE *list,int num)
+{
+
+    NODE *temp,*newnode;
+    for(temp=list;temp->next!=list;temp=temp->next);
+
+    newnode=(NODE*)malloc(sizeof(NODE));
+    newnode->data=num;
+
+temp->next=newnode;
+newnode->prev=temp;
+temp=newnode;
+temp->next=list;
+list->prev=temp;
+
+
+    return list;
+}
+
+int main(){
+    int num;
+struct node *list=NULL;
+list=create(list);
+disp(list);
+
+printf("\n enter node data to insert values.....");
+scanf("%d",&num);
+list=insertend(list,num);
+printf("\n end insert data list.....");
+disp(list);
+    return 0;
+}
